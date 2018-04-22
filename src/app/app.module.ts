23b49a2834c121e40cc869b2393303d4df5fbedc
MehdiDev78 <mehdi.dev.more@gmail.com>
2018-04-22 22:38:09 +0200

    finilisation du projet 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { DirectiveComponent } from './directive/directive.component';
+import { LiaisonsComponent } from './liaisons/liaisons.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+DirectiveComponent,
+LiaisonsComponent,
+
+],
+imports: [
+BrowserModule,
+FormsModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
